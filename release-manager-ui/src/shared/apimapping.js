44afa5adbfda948f9cfc044352ceab8346423bd6
anjanagnair <56006900+anjanagnair@users.noreply.github.com>
2021-06-03 11:32:27 +1200

    api mapping updated

--- release-manager-ui/src/shared/apimapping.js
+++ release-manager-ui/src/shared/apimapping.js
@@ -2,11 +2,15 @@ const GET_JIRA_ISSUES = "jira/getall";
 const GET_PROJECT_ISSUES = 'jira/getall/issues?project=Cakify-app&labels=test-release-1';
 const GET_PROJECTS = "project/getall";
 const CREATE_PROJECT = "project/register";
+const POST_RELEASES='jira/create/release'
+const GET_RELEASES='jira/get/all/releases'
 // const GET_PROJECT_ISSUES = `jira/getall/issues?project=${project}&labels=${label}`;
 
 export default {
     GET_PROJECTS,
     GET_PROJECT_ISSUES,
     GET_JIRA_ISSUES, 
-    CREATE_PROJECT
+    CREATE_PROJECT,
+    POST_RELEASES,
+    GET_RELEASES
 }
